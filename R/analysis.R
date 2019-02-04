00a1a26c5a91827ce8df4dd828fa61d60b32f8ab
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-02-04 12:02:14 -0800

    mention message as a logging option

--- R/analysis.R
+++ R/analysis.R
@@ -1,5 +1,6 @@
 # Your R script
 x = 1:10
 
-message("This message came from R.
+message("Adding messages like this is an easy way to add logging to your program.
+This will help you debug things that run in batch mode.
 Look for it in a file that ends with .out.")
