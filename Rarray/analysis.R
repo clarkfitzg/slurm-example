4d2eefd3ab101e0745701f0ccd48d816a8d19ba4
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-02-04 21:34:27 -0800

    going to run the R script shortly

--- Rarray/analysis.R
+++ Rarray/analysis.R
@@ -10,3 +10,9 @@ if(0 < job_number){
     for(i in seq(job_number)) seed = parallel::nextRNGStream(seed)
 }
 set.seed(seed)
+
+# Simulate 
+result = mean(rnorm(100))
+
+# Save the result for each run
+saveRDS(result, paste0(job_number, ".rds"))
